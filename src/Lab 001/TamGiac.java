333aca07d607e2b8cb0694f301f8cb78e24c10b5
kiwwi1 <dquanghuy221@gmail.com>
2024-09-28 08:47:47 +0700

    first commit

--- src/Lab 001/TamGiac.java
+++ src/Lab 001/TamGiac.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tamgiac {
+public class TamGiac {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
